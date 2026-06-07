3675ccb85599a5b0faaa2cb2e6b6dbdf6579603c
Devanshu <ks.devanshu@gmail.com>
2026-06-07 19:22:26 +0530

    Template Method Pattern

---
+++ TemplateMethodPattern/Exercise/Application.java
@@ -0,0 +1,13 @@
+package TemplateMethodPattern.Exercise;
+
+public class Application extends Window {
+    @Override
+    protected void onClosing() {
+        System.out.println("Before closing.");
+    }
+
+    @Override
+    protected void onClosed() {
+        System.out.println("After closing");
+    }
+}
---
+++ TemplateMethodPattern/Main.java
@@ -0,0 +1,10 @@
+package TemplateMethodPattern;
+
+public class Main {
+    public static void main(String[] args) {
+        Task taskOne = new TransferMoneyTask();
+        Task taskTwo = new GenerateReportTask(new AuditTrail());
+        taskOne.execute();
+        taskTwo.execute();
+    }
+}
